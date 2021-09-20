7cb395eac28c1c285264c01397d3221b29b076ba
Thien-An Mac <tmac@users.noreply.github.com>
2021-09-19 22:00:58 -0400

    Copy local zshrc to root folder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~/.zshrc
+source ~/.zshrc
 curl -L https://bit.ly/janus-bootstrap | bash
 brew bundle
