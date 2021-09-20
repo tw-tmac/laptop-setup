1100fab963883bc0615bc42827e37c5abe0996ae
Thien-An Mac <tmac@Thien-Ans-MacBook-Pro.local>
2021-09-19 21:55:19 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -L https://bit.ly/janus-bootstrap | bash
+brew bundle
