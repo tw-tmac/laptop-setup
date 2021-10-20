951b05c295aef85a153d35e247f1e36a4426cbc8
Thien-An Mac <tmac@users.noreply.github.com>
2021-10-20 13:37:30 -0400

    Fix script to not die after installing ohmyzsh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,11 @@ set -e
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Installing ohmyzsh.."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wait 1
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
+sleep 1
 cp .zshrc ~/.zshrc
-source ~/.zshrc
 echo "Installing janus"
 curl -L https://bit.ly/janus-bootstrap | bash
 echo "Installing essential packages..."
 brew bundle
-echo "Set up complete. If you want to install extra stuff, run brew bundle --file Brewfile_extra"
+echo "Set up complete. If you want to install extra stuff, run brew bundle --file Brewfile_extra. You might also need to source ~/.zshrc as well"
